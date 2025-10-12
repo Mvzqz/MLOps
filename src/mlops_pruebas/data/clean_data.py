017814e584a3d8fa1148eb098934ede730f6dad0
Manuel Vázquez <manuel.vazquez.meza@gmail.com>
2025-10-12 16:17:53 -0600

    sets up dvc on gdrive, tracks data files and creates dvc stages

--- src/mlops_pruebas/data/clean_data.py
+++ src/mlops_pruebas/data/clean_data.py
@@ -1,1 +1,29 @@
 """Data cleaning module for the MLOps project."""
+
+import pandas as pd
+
+
+def clean_data(input_path: str, output_path: str) -> None:
+    """Cleans the raw data and saves the cleaned data to the specified output path.
+
+    Parameters:
+    - input_path: str - Path to the raw data file.
+    - output_path: str - Path where the cleaned data will be saved.
+    """
+
+    # Load raw data
+    df = pd.read_csv(input_path)
+
+    # Save cleaned data
+    df.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 3:
+        print("Usage: python clean_data.py <input_path> <output_path>")
+    else:
+        input_path = sys.argv[1]
+        output_path = sys.argv[2]
+        clean_data(input_path, output_path)
--- src/mlops_pruebas/features/build_features.py
+++ src/mlops_pruebas/features/build_features.py
@@ -1,1 +1,31 @@
 """Feature engineering module for the MLOps project."""
+
+import pandas as pd
+
+
+def build_features(input_path: str, output_path: str) -> None:
+    """Builds features from the raw data and saves the features to the specified output
+    path.
+
+    Parameters:
+    - input_path: str - Path to the raw data file.
+    - output_path: str - Path where the features will be saved.
+    """
+
+    # Load clean data
+    df = pd.read_csv(input_path)
+
+    # Feature engineering steps
+    # Save features
+    df.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 3:
+        print("Usage: python build_features.py <input_path> <output_path>")
+    else:
+        input_path = sys.argv[1]
+        output_path = sys.argv[2]
+        build_features(input_path, output_path)
