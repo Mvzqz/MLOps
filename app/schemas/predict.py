a1620e4e9bfc57713bb1ed9ee1bb29fde1e2faed
Manuel Vázquez <manuel.vazquez.meza@gmail.com>
2025-11-16 21:10:37 -0600

    adds  examples to swager

--- app/schemas/predict.py
+++ app/schemas/predict.py
@@ -3,21 +3,20 @@ from pydantic import BaseModel, Field
 from typing import List, Union
 
 class FeatureRow(BaseModel):
-    Date: str
-    Hour: float
-    Temperature_C: float
-    Humidity: float
-    Wind_speed_m_s: float
-    Visibility_10m: float
-    Dew_point_temperature_c: float
-    solar_radiation_mj_m2: float
-    rainfall_mm: str
-    snowfall_cm: str
-    Seasons: str
-    Holiday: str
-    Functioning_Day: str
-    mixed_type_col: str
-
+    Date: str = Field(..., example="01/12/2017")
+    Hour: float = Field(..., example=0.0)
+    Temperature_C: float = Field(..., example=-5.2)
+    Humidity: float = Field(..., example=37.0)
+    Wind_speed_m_s: float = Field(..., example=2.2)
+    Visibility_10m: float = Field(..., example=2000.0)
+    Dew_point_temperature_c: float = Field(..., example=-17.6)
+    solar_radiation_mj_m2: float = Field(..., example=0.0)
+    rainfall_mm: float = Field(..., example=0.0)
+    snowfall_cm: float = Field(..., example=0.0)
+    Seasons: str = Field(..., example="Winter")
+    Holiday: str = Field(..., example="No Holiday")
+    Functioning_Day: str = Field(..., example="Yes")
+    mixed_type_col: str = Field(..., example="873") 
 class PredictRequest(BaseModel):
     Features: List[FeatureRow]
 
